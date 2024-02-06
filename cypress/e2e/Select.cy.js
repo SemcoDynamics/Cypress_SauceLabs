e91d85ef4666c5c94e5fe1aa660efad8ae8da3fa
Brandon Olckers <bolckers19@gmail.com>
2024-02-06 20:25:52 +0200

    first commit

--- cypress/e2e/Select.cy.js
+++ cypress/e2e/Select.cy.js
@@ -1,34 +1,41 @@
 /// <reference types="cypress" />
 
 describe('Select Single and Multi-Selects', () => {
-    it('Select a Single value from the drop down', () => {
-      cy.visit('https://kitchen.applitools.com/ingredients/select');
+  it('Select a Single value from the drop down', () => {
+    cy.visit('https://kitchen.applitools.com/ingredients/select');
+  
+    const searchEntry = ['chili-powder', 'garlic', 'ginger', 'paprika'];
+    const indexToSelect = 3
+    cy.get('#spices-select-single').select(searchEntry[indexToSelect]);
     
+    // Verify that the selected value is 'paprika'
+    cy.get('#spices-select-single').should('have.value', searchEntry[indexToSelect]);
+    
+    // Optionally, you can also check if the selected value is one of the values in the searchEntry array
+    cy.get('#spices-select-single').invoke('val').should('include', searchEntry[indexToSelect]);
+  });
+
+  it('Loop through each Single Selection', () => {
+      cy.visit('https://kitchen.applitools.com/ingredients/select');
+
       const searchEntry = ['chili-powder', 'garlic', 'ginger', 'paprika'];
-      const indexToSelect = 3
-      cy.get('#spices-select-single').select(searchEntry[indexToSelect]);
-      
-      // Verify that the selected value is 'paprika'
-      cy.get('#spices-select-single').should('have.value', searchEntry[indexToSelect]);
-      
-      // Optionally, you can also check if the selected value is one of the values in the searchEntry array
-      cy.get('#spices-select-single').invoke('val').should('include', searchEntry[indexToSelect]);
-    });
+      // Loop through each value in the searchEntry array
+      searchEntry.forEach((value) => {
+          // Select the current value from the drop-down
+          cy.get('#spices-select-single').select(value);
 
-    it.only('Loop through each value', () => {
-        cy.visit('https://kitchen.applitools.com/ingredients/select');
+          // Verify that the selected value matches the expected value
+          cy.get('#spices-select-single').should('have.value', value);
 
-        const searchEntry = ['chili-powder', 'garlic', 'ginger', 'paprika'];
-        // Loop through each value in the searchEntry array
-        searchEntry.forEach((value) => {
-            // Select the current value from the drop-down
-            cy.get('#spices-select-single').select(value);
-
-            // Verify that the selected value matches the expected value
-            cy.get('#spices-select-single').should('have.value', value);
-
-            // Optionally, you can also check if the selected value is one of the values in the searchEntry array
-            cy.get('#spices-select-single').invoke('val').should('include', value);
-        })
+          // Optionally, you can also check if the selected value is one of the values in the searchEntry array
+          cy.get('#spices-select-single').invoke('val').should('include', value);
+      })
   })
 });
+describe('Multi Selectiopns', () => {
+  it.only('Select multiple selection', () => {
+    cy.visit('https://kitchen.applitools.com/ingredients/select');
+    cy.get('#spices-select-multi').select(['chili-powder', 'garlic']).invoke('val').should('deep.equal',['chili-powder', 'garlic'] )
+
+  })
+})
